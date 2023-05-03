ddea62ac29e77e9858689393ba23a0dc2ea6bf78
Ali <alin89328@gmail.com>
2023-05-03 17:56:50 +0500

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Api Running"));
+app.get("/", (req, res) => res.send("Api Running correctly!"));
 
 const PORT = process.env.PORT || 5000;
 
