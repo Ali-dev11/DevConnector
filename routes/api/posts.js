1baa529e18ed939596521e9a79e9e54aee2c0121
Ali <alin89328@gmail.com>
2023-05-22 16:38:30 +0500

    feat: Added delete post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,5 +71,31 @@ router.get("/:id", auth, async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: "Post not found" });
+        }
+
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" });
+        }
+
+        await Post.findOneAndDelete({ _id: req.params.id });
+
+        res.json({ msg: "Post removed" });
+    } catch (err) {
+        console.error(err.message);
+
+        res.status(500).send("Server Error");
+    }
+});
   
 module.exports = router;
