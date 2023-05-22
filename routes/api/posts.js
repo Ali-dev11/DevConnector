ba0b4bd95a93e590d565bd431bfee20df4f42ac7
Ali <alin89328@gmail.com>
2023-05-22 17:04:48 +0500

    feat: Added like and unlike post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,5 +97,54 @@ router.delete("/:id", auth, async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+router.put("/like/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has already been liked
+        if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+            return res.status(400).json({ msg: "Post already liked" });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        return res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+router.put("/unlike/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has not yet been liked
+        if (!post.likes.some((like) => like.user.toString() === req.user.id)) {
+            return res.status(400).json({ msg: "Post has not yet been liked" });
+        }
+
+        // remove the like
+        post.likes = post.likes.filter(
+            ({ user }) => user.toString() !== req.user.id
+        );
+
+        await post.save();
+
+        return res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
   
 module.exports = router;
